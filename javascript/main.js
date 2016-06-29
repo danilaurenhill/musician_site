abdb20f7463fa34b9b7e6e80c260d506e092a76f
danilaurenhill <dani.lauren.hill@gmail.com>
2016-06-29 14:02:13 -0400

    Adds php

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,18 @@ function hideshow(content1) {
   if (content1.style.display == "none") content1.style.display = "block"
   else content1.style.display = "none"
 }
+function hideshow(content2) {
+  if (!document.getElementById) return
+  if (content2.style.display == "none") content2.style.display = "block"
+  else content2.style.display = "none"
+}
+function hideshow(content3) {
+  if (!document.getElementById) return
+  if (content3.style.display == "none") content3.style.display = "block"
+  else content3.style.display = "none"
+}
+function hideshow(content4) {
+  if (!document.getElementById) return
+  if (content4.style.display == "none") content4.style.display = "block"
+  else content4.style.display = "none"
+}
