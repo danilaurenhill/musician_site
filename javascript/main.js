57f49bb75e86b89275b3b9da27562cb4e9058fee
danilaurenhill <dani.lauren.hill@gmail.com>
2016-08-14 13:22:27 -0400

    finished bio page

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,20 @@
-function hideshow(content1) {
-  if (!document.getElementById) return
-  if (content1.style.display == "none") content1.style.display = "block"
-  else content1.style.display = "none"
-}
-function hideshow(content2) {
-  if (!document.getElementById) return
-  if (content2.style.display == "none") content2.style.display = "block"
-  else content2.style.display = "none"
-}
-function hideshow(content3) {
-  if (!document.getElementById) return
-  if (content3.style.display == "none") content3.style.display = "block"
-  else content3.style.display = "none"
-}
-function hideshow(content4) {
-  if (!document.getElementById) return
-  if (content4.style.display == "none") content4.style.display = "block"
-  else content4.style.display = "none"
-}
+document.getElementById('hamburgler').addEventListener('click', checkNav);
+  window.addEventListener("keyup", function(e) {
+    if (e.keyCode == 27) closeNav();
+  }, false);
+
+  function checkNav() {
+    if (document.body.classList.contains('hamburgler-active')) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  }
+
+  function closeNav() {
+    document.body.classList.remove('hamburgler-active');
+  }
+
+  function openNav() {
+    document.body.classList.add('hamburgler-active');
+  }
